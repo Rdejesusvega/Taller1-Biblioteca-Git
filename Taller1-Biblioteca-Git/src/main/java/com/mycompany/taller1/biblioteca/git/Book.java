a1a6aab3c25a6d1dc8a3c10053989db437b59b59
Rdejesusvega <Rdejesusvega@unicesar.edu.co>
2026-08-18 22:24:46 -0500

    Update Book.java

--- Taller1-Biblioteca-Git/src/main/java/com/mycompany/taller1/biblioteca/git/Book.java
+++ Taller1-Biblioteca-Git/src/main/java/com/mycompany/taller1/biblioteca/git/Book.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.taller1.biblioteca.git;
-
 /**
- *
- * @author v7434
+ * Book
+ * Inherits attributes from Material (code, title, publicationYear)
+ * and adds its own attributes: author, available.
  */
-public class Book {
-    
+public class Book extends Material {
+
+    // Own attributes
+    private String author;
+    private boolean available;
+
+    // Constructor
+    public Book(String code, String title, String publicationYear, String author, boolean available) {
+        super(code, title, publicationYear);
+        this.author = author;
+        this.available = available;
+    }
+
+    // Getters
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    // Setters
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    // toString for easy printing/debugging
+    @Override
+    public String toString() {
+        return "Book{" +
+                "code='" + code + '\'' +
+                ", title='" + title + '\'' +
+                ", publicationYear='" + publicationYear + '\'' +
+                ", author='" + author + '\'' +
+                ", available=" + available +
+                '}';
+    }
 }

73575d0b175483676ca3eaf9ae4850efd43a5339
Rdejesusvega <rdejesusvega@unicesar.edu.co>
2026-08-18 19:24:44 -0500

    feat: crear clase Persona como padre de cliente

--- Taller1-Biblioteca-Git/src/main/java/com/mycompany/taller1/biblioteca/git/Taller1BibliotecaGit.java
+++ Taller1-Biblioteca-Git/src/main/java/com/mycompany/taller1/biblioteca/git/Taller1BibliotecaGit.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.taller1.biblioteca.git;
 
-/**
- *
- * @author v7434
- */
 public class Taller1BibliotecaGit {
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
